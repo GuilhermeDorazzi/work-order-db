a2f40f44e86d439941690621d03f1037a56fbaa3
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2023-04-30 18:41:02 -0300

    Limpeza nos scripts e movendo para a pasta de executados em produção

---
+++ Executados/05_INSERT_PAGES_20230431.sql
@@ -0,0 +1,3 @@
+--
+INSERT INTO `tb_paginas` (`COD_PAGINA`, `NOME_PAGINA`, `NOME_CONTROLADOR`, `FLG_ATIVO`, `COD_TIPO`, `FLG_TIPO_PAGINA`) VALUES ('2003', 'Tech Orders', 'CD_TechOrders', 'S', '200', 'C');
+--
