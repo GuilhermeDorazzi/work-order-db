028d52893504825c675c534623e69bfa8d2ef238
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2023-06-17 18:19:28 -0300

    Ajuste para renomear as Paginas do sistema

---
+++ Executados/020_UPDATE_TB_PAGINAS_20230615.sql
@@ -0,0 +1,5 @@
+--
+UPDATE `tb_paginas` SET `NOME_PAGINA` = 'Job Code' WHERE (`COD_PAGINA` = '4002');
+UPDATE `tb_paginas` SET `NOME_PAGINA` = 'Market' WHERE (`COD_PAGINA` = '4004');
+UPDATE `tb_paginas` SET `NOME_PAGINA` = 'Tech x Market' WHERE (`COD_PAGINA` = '4005');
+--
