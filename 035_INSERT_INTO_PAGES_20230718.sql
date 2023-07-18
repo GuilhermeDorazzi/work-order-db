ae79f38a41a4f3dfc6ecdceb9b8c9d91f5b9d480
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2023-07-18 19:57:06 -0300

    Alteração que contempla a criação das tabelas que vão realizar o processo de avisos.

---
+++ 035_INSERT_INTO_PAGES_20230718.sql
@@ -0,0 +1,10 @@
+--
+INSERT INTO tb_tipos_paginas (COD_TIPO, CLASSE_CSS_ICON, DESCRICAO, ORDEM_MENU) 
+VALUES ('500', ' fa-comment-alt-exclamation ', 'Notice', '5');
+--
+INSERT INTO tb_paginas (COD_PAGINA, NOME_PAGINA, NOME_CONTROLADOR, FLG_ATIVO, COD_TIPO, FLG_TIPO_PAGINA, NIVEL_ACESSO) 
+VALUES ('5001', 'Notice', 'CD_Aviso', 'S', '500', 'C', '98');
+--
+INSERT INTO tb_paginas (COD_PAGINA, NOME_PAGINA, NOME_CONTROLADOR, FLG_ATIVO, COD_TIPO, FLG_TIPO_PAGINA, NIVEL_ACESSO) 
+VALUES ('5002', 'Accepted Terms', 'CD_AvisoUsuario', 'S', '500', 'C', '1');
+--
