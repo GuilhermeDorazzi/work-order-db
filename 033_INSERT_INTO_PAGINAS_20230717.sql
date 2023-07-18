b6e74a29cd8e8142b3d7d77681375365b9c4ea6b
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2023-07-18 11:30:28 -0300

    Criação do script para cadastrar a nova tela de relatório do QC

---
+++ 033_INSERT_INTO_PAGINAS_20230717.sql
@@ -0,0 +1,4 @@
+--
+INSERT INTO tb_paginas (COD_PAGINA, NOME_PAGINA, NOME_CONTROLADOR, FLG_ATIVO, COD_TIPO, FLG_TIPO_PAGINA, NIVEL_ACESSO) 
+VALUES ('3014', 'Report Simple 2 (QC)', 'CD_ReportsSimple2', 'S', '300', 'C', '5');
+--
