349603570662c59f476bebc88b56be6b67ebe09b
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2023-07-29 14:46:30 -0300

    Alteração para mover a tela para outro grupo de menu e também a inclusão das tela de relatório do fild manager

--- 043_INSERT_TB_PAGINAS_TELA_FILD_MANAGER_20230728.sql
+++ 043_INSERT_TB_PAGINAS_TELA_FILD_MANAGER_20230728.sql
@@ -1,4 +1,10 @@
 --
 INSERT INTO tb_paginas (COD_PAGINA, NOME_PAGINA, NOME_CONTROLADOR, FLG_ATIVO, COD_TIPO, FLG_TIPO_PAGINA, NIVEL_ACESSO) 
-VALUES ('1008', 'Fild Manager X Tech', 'CD_FildManager', 'S', '100', 'M', '98');
+VALUES ('3015', 'Fild Manager X Tech', 'CD_FildManager', 'S', '300', 'M', '98');
+--
+INSERT INTO tb_paginas (COD_PAGINA, NOME_PAGINA, NOME_CONTROLADOR, FLG_ATIVO, COD_TIPO, FLG_TIPO_PAGINA, NIVEL_ACESSO) 
+VALUES ('3016', 'Admin Review Report (Fild Manager)', 'CD_ReportsAdminReviewReportFildManager', 'S', '300', 'C', '98');
+--
+INSERT INTO tb_paginas (COD_PAGINA, NOME_PAGINA, NOME_CONTROLADOR, FLG_ATIVO, COD_TIPO, FLG_TIPO_PAGINA, NIVEL_ACESSO) 
+VALUES ('3017', 'Admin Master Report Tech Job  (Fild Manager)', 'CD_ReportsAdminMasterReportTechJobFildManager', 'S', '300', 'C', '98');
 --
