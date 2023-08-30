810ab26ffaa1dc17d8b92fcbaa387d41521894b6
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2023-08-29 21:01:50 -0300

    Criação dos relatórios do field manager e também da tabela de dividas para colocar o credito

--- Scripts Complementares/001_AJUSTE_PARA_ALTERAR_OS_BD08_PARA_CALCULAR_CORRETO_20230819.sql
+++ Scripts Complementares/001_AJUSTE_PARA_ALTERAR_OS_BD08_PARA_CALCULAR_CORRETO_20230819.sql
@@ -3,25 +3,37 @@
 --
 -- Vamos criar a tabela de backup
 create table tb_ordem_bk_bd08
-select id_ordem, cod_empresa, data_baixa from tb_ordem 
+select id_ordem, cod_empresa, cod_filial, data_baixa from tb_ordem 
         where cod_empresa = 3 
+		  and cod_filial in (3,4)
+          and date(DATA_BAIXA) > '2023-07-24'
           and ID_ORDEM in (select distinct id_ordem from tb_jobs_ordem where COD_EMPRESA = 3 and COD_JOB in (198,199) and qtd2 is null) 
           and STATUS = 'Finished';
 --
 -- Vamos fazer o processo de alterar os status das ordens
 --
+SET SQL_SAFE_UPDATES = 0;
 update tb_ordem 
 set STATUS = 'Pending'
-where id_ordem in (select distinct id_ordem from tb_jobs_ordem where COD_EMPRESA = 3 and COD_JOB in (198,199) and qtd2 is null);
+where cod_filial in (3,4)
+  and date(DATA_BAIXA) > '2023-07-24'
+  and id_ordem in (select distinct id_ordem from tb_ordem_bk_bd08 where COD_EMPRESA = 3);
+SET SQL_SAFE_UPDATES = 1;
 --
 -- Fazer o processo manual de fechamento das ordens
 --
 update tb_ordem as od
-join tb_ordem_bk_bd08 as bk
-on od.id_ordem = bk.id_ordem
-and od.cod_empresa = bk.cod_empresa
-set os.data_baixa = bk.data_baixa
-where od.cod_empresa = 3;
+  join tb_ordem_bk_bd08 as bk
+    on od.id_ordem = bk.id_ordem
+   and od.cod_empresa = bk.cod_empresa
+   set od.data_baixa = bk.data_baixa
+ where od.cod_empresa = 3;
 --
 -- Vamos apagar a tabela com os backups
 -- drop table tb_ordem_bk_bd08;
+
+
+select * from tb_ordem where job_num = '110474061';
+select * from tb_jobs_ordem where ID_ORDEM = 11046;
+
+select * from tb_jobs_ordem where qtd2 is not null and qtd <> qtd2;
--- Scripts Complementares/002_UPADTE_RETROATIVO_DE_ORDENS_20230819.sql
+++ Scripts Complementares/002_UPADTE_RETROATIVO_DE_ORDENS_20230819.sql
@@ -4,6 +4,27 @@
 -- create table temp_tb_jobs_usuario select * from tb_jobs_usuario where cod_empresa = 3 and cod_usuario = 138; -- 17,32,33,16
 --
 --
+-- create table tb_jobs_ordem_bk_1908 select * from tb_jobs_ordem;
+-- 
+-- Filiais - 8,3,4
+-- Fields  - 105,119,138,146,164,176
+-- Empresa - 3
+--
+/*update tb_jobs_usuario as ju
+  join tb_job as jo
+    on jo.cod_empresa = ju.cod_empresa
+   and jo.cod_filial = ju.cod_filial
+   and jo.cod_job = ju.cod_job
+   set ju.valor_rep = jo.valor_rep
+ where ju.cod_empresa = 3
+   and ju.cod_filial = 6
+   and (ju.cod_usuario in (select COD_USUARIO from tb_usuarios where cod_empresa = 3 and FLG_FILD_MANAGER = 'S')
+   or ju.cod_usuario in (select COD_USUARIO from tb_usuarios where cod_empresa = 3 and COD_FILD_MANAGER IN (105, 119, 138, 146, 164))
+   );*/
+--
+-- Filiais - 8,3,4
+-- Fields  - 105,119,138,146,164,176
+-- Empresa - 3
 update tb_jobs_ordem as jo
   join tb_jobs_usuario as ju
     on ju.COD_EMPRESA = jo.COD_EMPRESA
@@ -13,6 +34,11 @@ update tb_jobs_ordem as jo
 set jo.VALOR_REP = ju.VALOR_REP,
     jo.VALOR_REC = ju.VALOR_REC,
     jo.VALOR_PAG = ju.VALOR_PAG
-where jo.COD_EMPRESA = 3 
--- and jo.COD_FILIAL = 4
-and ju.COD_USUARIO in (17,32,33,16);
+where jo.COD_EMPRESA = 3
+ and jo.COD_FILIAL = 6
+  and (jo.cod_usuario in (select COD_USUARIO from tb_usuarios where cod_empresa = 3 and FLG_FILD_MANAGER = 'S')
+   or jo.cod_usuario in (select COD_USUARIO from tb_usuarios where cod_empresa = 3 and COD_FILD_MANAGER IN (105, 119, 138, 146, 164))
+   );
+
+
+-- select * from tb_usuarios where COD_USUARIO in (17,32,33,16);
