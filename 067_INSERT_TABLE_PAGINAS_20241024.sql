c31987b2716c25eb70779e0c1715abf97979854a
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2024-10-24 22:41:48 -0300

    Movendo script e criando novos

---
+++ 067_INSERT_TABLE_PAGINAS_20241024.sql
@@ -0,0 +1,3 @@
+--
+INSERT INTO tb_paginas (`COD_PAGINA`, `NOME_PAGINA`, `NOME_CONTROLADOR`, `FLG_ATIVO`, `COD_TIPO`, `FLG_TIPO_PAGINA`, `NIVEL_ACESSO`) 
+	 VALUES ('3024', 'Salary Report', 'CD_SalaryReport', 'S', '300', 'C', '99');
