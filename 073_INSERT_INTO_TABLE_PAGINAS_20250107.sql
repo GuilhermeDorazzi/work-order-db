b700f73f383392d388451086e6a0cf3119769c8a
Guilherme Dorazzi <guilhermedorazzi@gmail.com>
2025-01-07 11:31:07 -0300

    Ajuste que contempla a cricacao do novo relatorio.

---
+++ 073_INSERT_INTO_TABLE_PAGINAS_20250107.sql
@@ -0,0 +1,4 @@
+--
+INSERT INTO `tb_paginas` (`COD_PAGINA`, `NOME_PAGINA`, `NOME_CONTROLADOR`, `FLG_ATIVO`, `COD_TIPO`, `FLG_TIPO_PAGINA`, `NIVEL_ACESSO`) 
+     VALUES ('3035', 'Submanager Report', 'CD_ReportsSubmanagerReport', 'S', '300', 'C', '98');
+--
